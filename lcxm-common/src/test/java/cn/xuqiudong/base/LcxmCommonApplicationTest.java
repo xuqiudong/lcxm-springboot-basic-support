0f7a89a7ad1c79738dcd5e1fd41682690062f325
临窗旋墨 <xuduochoua@163.com>
2025-07-31 11:38:56 +0800

    feat(common): LcxmCommonPropertiesPostProcessor 默认配置项

---
+++ lcxm-common/src/test/java/cn/xuqiudong/base/LcxmCommonApplicationTest.java
@@ -0,0 +1,18 @@
+package cn.xuqiudong.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述:
+ *
+ * @author Vic.xu
+ * @since 2025-07-30 9:34
+ */
+@SpringBootApplication
+public class LcxmCommonApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcxmCommonApplicationTest.class, args);
+    }
+}
