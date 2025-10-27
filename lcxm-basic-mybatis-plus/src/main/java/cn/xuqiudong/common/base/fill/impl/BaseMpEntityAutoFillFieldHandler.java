a3ad4a0d322e85e024935bc72957bcd6286dd3a2
临窗旋墨 <xuduochoua@163.com>
2025-10-27 14:54:52 +0800

    mybatis-plus 的BaseMpEntity 类型的实体属性填充处理

--- lcxm-basic-mybatis-plus/src/main/java/cn/xuqiudong/common/base/fill/impl/BaseMpEntityAutoFillFieldHandler.java
+++ lcxm-basic-mybatis-plus/src/main/java/cn/xuqiudong/common/base/fill/impl/BaseMpEntityAutoFillFieldHandler.java
@@ -2,7 +2,12 @@ package cn.xuqiudong.common.base.fill.impl;
 
 import cn.xuqiudong.common.base.entity.BaseMpEntity;
 import cn.xuqiudong.common.base.fill.AutoFillFieldHandler;
+import cn.xuqiudong.common.helper.CurrentUserInfoHelper;
 import org.apache.ibatis.reflection.MetaObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
 
 /**
  * 描述:
@@ -12,11 +17,38 @@ import org.apache.ibatis.reflection.MetaObject;
  * @since 2025-09-10 14:24
  */
 public class BaseMpEntityAutoFillFieldHandler implements AutoFillFieldHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseMpEntityAutoFillFieldHandler.class);
+
+    public static final String TIPS = "未提供获取当前登录用户的方法, 将使用匿名用户";
+
     @Override
     public void insertFill(MetaObject metaObject) {
         Object entity = metaObject.getOriginalObject();
         if (entity instanceof BaseMpEntity baseMpEntity) {
-            //TODO
+            tips();
+
+            String useId = CurrentUserInfoHelper.getUserId();
+            // 字段为空的时候 设置相关默认值
+
+            // 创建人
+            if (baseMpEntity.getCreateBy() == null) {
+                baseMpEntity.setCreateBy(useId);
+            }
+            // 创建时间
+            if (baseMpEntity.getCreateTime() == null) {
+                baseMpEntity.setCreateTime(LocalDateTime.now());
+            }
+
+            // 修改人
+            if (baseMpEntity.getUpdateBy() == null) {
+                baseMpEntity.setUpdateBy(useId);
+            }
+            // 修改时间
+            if (baseMpEntity.getUpdateTime() == null) {
+                baseMpEntity.setUpdateTime(LocalDateTime.now());
+            }
+
         }
 
     }
@@ -25,8 +57,18 @@ public class BaseMpEntityAutoFillFieldHandler implements AutoFillFieldHandler {
     public void updateFill(MetaObject metaObject) {
         Object entity = metaObject.getOriginalObject();
         if (entity instanceof BaseMpEntity baseMpEntity) {
-            //TODO
+            tips();
+            String useId = CurrentUserInfoHelper.getUserId();
+            // 总是更新 修改人和修改时间
+            baseMpEntity.setUpdateBy(useId);
+            baseMpEntity.setUpdateTime(LocalDateTime.now());
         }
 
+    }
+
+    private void tips() {
+        if (CurrentUserInfoHelper.isAnonymous()) {
+            LOGGER.warn(TIPS);
+        }
     }
 }
