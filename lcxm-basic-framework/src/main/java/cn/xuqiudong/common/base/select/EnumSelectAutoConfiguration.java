3400324fadb665abb1dac098671aac21e935c8b6
临窗旋墨 <xuduochoua@163.com>
2025-11-14 10:36:22 +0800

    新增枚举转下拉框模块的自动配置模块

--- lcxm-basic-framework/src/main/java/cn/xuqiudong/common/base/select/EnumSelectAutoConfiguration.java
+++ lcxm-basic-framework/src/main/java/cn/xuqiudong/common/base/select/EnumSelectAutoConfiguration.java
@@ -1,17 +1,20 @@
 package cn.xuqiudong.common.base.select;
 
 import cn.xuqiudong.common.base.select.controller.EnumSelectController;
+import cn.xuqiudong.common.base.select.scan.EnumSelectScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 描述:
  * 枚举转下拉框模块的自动配置
- *
+ * 枚举包配置: lcxm.framework.enum.scan-base-packages=cn.xuqiudong.test,cn.xuqiudong.demo
  * @author Vic.xu
  * @since 2025-11-13 17:59
  */
@@ -20,6 +23,7 @@ import org.springframework.context.annotation.Configuration;
 public class EnumSelectAutoConfiguration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EnumSelectAutoConfiguration.class);
+
 
     public EnumSelectAutoConfiguration() {
         LOGGER.info("启用枚举转下拉框模块");
@@ -32,5 +36,16 @@ public class EnumSelectAutoConfiguration {
     @ConditionalOnMissingBean
     public EnumSelectController enumSelectController() {
         return new EnumSelectController();
+    }
+
+    /**
+     * 扫描枚举 注册到 EnumSelectRegistry
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public EnumSelectScanner enumSelectScanner(
+            ApplicationContext applicationContext,
+            @Value("${lcxm.framework.enum.scan-base-packages:}") String scanPackage) {
+        return new EnumSelectScanner(applicationContext, scanPackage);
     }
 }
