43e50b171d27c97c8bef183ee998ae7f79dc17c4
临窗旋墨 <xuduochoua@163.com>
2026-01-16 16:38:13 +0800

    不再自动注册RepeatCommitVersionAspect

--- lcxm-basic-framework/src/main/java/cn/xuqiudong/common/base/aspect/RepeatCommitVersionAspect.java
+++ lcxm-basic-framework/src/main/java/cn/xuqiudong/common/base/aspect/RepeatCommitVersionAspect.java
@@ -3,6 +3,8 @@ package cn.xuqiudong.common.base.aspect;
 import cn.xuqiudong.common.base.aspect.annotation.RepeatCommitVersion;
 import cn.xuqiudong.common.base.enums.CommonMsgEnum;
 import cn.xuqiudong.common.base.exception.CommonException;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -11,10 +13,6 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
-
-import jakarta.annotation.Resource;
-import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * 描述:form重复提交版本检测，    依赖于数据库字段 ;
@@ -24,7 +22,6 @@ import jakarta.servlet.http.HttpServletRequest;
  * @since 2022-03-16 10:27
  */
 @Aspect
-@Component
 public class RepeatCommitVersionAspect {
 
     @Resource
