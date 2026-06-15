6ae2e65347e1d85e609b31ba84fdacd3156a3e36
临窗旋墨 <xuduochoua@163.com>
2026-06-15 13:20:44 +0800

    SpringBeanJobFactory

--- lcxm-spring-boot-starters/lcxm-quartz-spring-boot-starter/src/main/java/cn/xuqiudong/basic/quartz/autoconfigure/LcxmQuartzAutoConfiguration.java
+++ lcxm-spring-boot-starters/lcxm-quartz-spring-boot-starter/src/main/java/cn/xuqiudong/basic/quartz/autoconfigure/LcxmQuartzAutoConfiguration.java
@@ -7,17 +7,25 @@ import cn.xuqiudong.basic.quartz.runner.TaskJobInitializationRunner;
 import cn.xuqiudong.basic.quartz.service.TaskJobService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.quartz.Scheduler;
+import org.quartz.spi.TriggerFiredBundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
+import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
+import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 描述:
@@ -42,6 +50,31 @@ public class LcxmQuartzAutoConfiguration {
         LOGGER.info("init 自定义的定时器starter配置类");
     }
 
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SpringBeanJobFactory springBeanJobFactory(AutowireCapableBeanFactory beanFactory) {
+        return new SpringBeanJobFactory() {
+            @Override
+            protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+                Object job = super.createJobInstance(bundle);
+                // 注入 Spring Bean
+                beanFactory.autowireBean(job);
+                return job;
+            }
+        };
+    }
+
+    /**
+     * 注入到SpringBoot创建的SchedulerFactoryBean中
+     * 而不是注解注册 bean  SchedulerFactoryBean 不然需要标注为Primary，且可能丢失一些配置
+     * @see QuartzAutoConfiguration#quartzScheduler(QuartzProperties, ObjectProvider, ObjectProvider, Map, ObjectProvider, ApplicationContext)
+     */
+    @Bean
+    public SchedulerFactoryBeanCustomizer quartzCustomizer(SpringBeanJobFactory springBeanJobFactory) {
+        return schedulerFactoryBean ->
+                schedulerFactoryBean.setJobFactory(springBeanJobFactory);
+    }
     @Bean
     @ConditionalOnMissingBean
     public UnifyTaskEntry unifyTaskEntry(List<AbstractTaskJob> taskJobList) {
