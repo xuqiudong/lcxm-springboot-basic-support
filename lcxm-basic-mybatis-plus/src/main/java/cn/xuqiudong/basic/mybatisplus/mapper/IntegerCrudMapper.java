5eb2959ec7adb695d90810d91f5503c502bc085e
临窗旋墨 <xuduochoua@163.com>
2026-06-24 15:25:26 +0800

    IntegerCrudMapper

---
+++ lcxm-basic-mybatis-plus/src/main/java/cn/xuqiudong/basic/mybatisplus/mapper/IntegerCrudMapper.java
@@ -0,0 +1,12 @@
+package cn.xuqiudong.basic.mybatisplus.mapper;
+
+/**
+ * Description:
+ * Integer 类型的主键的Mapper， 基于mybatis-plus
+ *
+ * @author Vic.xu
+ * @since 2026-06-24 10:09
+ */
+public interface IntegerCrudMapper<T> extends MpGenericMapper<Integer, T> {
+
+}
